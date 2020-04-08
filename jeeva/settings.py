8c6f51157bf1407cf751a05c24c957679adc5b92
naveengct <naveenrajesh2222@gmmail.com>
2020-04-08 16:28:34 +0530

    first2

--- jeeva/settings.py
+++ jeeva/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.GzipManifestStaticFilesStorage'
 
 
 ALLOWED_HOSTS = ['hellojeeva.herokuapp.com']
